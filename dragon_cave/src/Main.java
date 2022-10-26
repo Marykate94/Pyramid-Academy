d0b0878d401279ea2ea80d8e89b6276d7d011be0
Marykate Smith <Marykatesmith64@gmail.com>
2022-10-25 21:08:41 -0400

    edited prompt

--- dragon_cave/src/Main.java
+++ dragon_cave/src/Main.java
@@ -1,13 +1,12 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-
-
+        
         System.out.println("You are in a land full of dragons." +
                 " The dragons all live in caves with their large piles of collected treasure." +
                 "Some dragons are friendly and share their treasure." +
                 "Other dragons are hungry and eat anyone who enters their cave." +
                 "You approach two caves, one with a friendly dragon and the other with a hungry dragon." +
-                "Which of the two caves do you choose?" );
+                "Which of the two caves do you choose? (1 or 2)" );
     }
 }
