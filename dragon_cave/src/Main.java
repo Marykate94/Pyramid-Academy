f134a921cad9463eaefa5ece1ac5376865e801d6
Marykate Smith <Marykatesmith64@gmail.com>
2022-10-25 20:48:43 -0400

    psuedocoded

--- dragon_cave/src/Main.java
+++ dragon_cave/src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
+        //player approaches two caves, one friendly & one hungry
+        // player input is in bold
+        // which cave do you go into, 1 or 2?
+        // hungry dragon eats you - process finishes with exit code 0
+        
         System.out.println("Hello world!");
     }
 }
