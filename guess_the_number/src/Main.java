94652ffc35a5812253f47d52dbce0e711d9fc6b8
Marykate Smith <Marykatesmith64@gmail.com>
2022-10-31 18:34:17 -0400

    cleaned up code

--- guess_the_number/src/Main.java
+++ guess_the_number/src/Main.java
@@ -14,14 +14,11 @@ public class Main {
         Random theNumber = new Random();
         int correctNumber = theNumber.nextInt(20);
         int turns = 0;
-
         Scanner scan = new Scanner(System.in);
         System.out.println("Take a guess.");
 
         int guess;
         boolean win =false;
-//        String playAgain = "";
-//        boolen play = true;
 
         while (win == false) {
             guess = scan.nextInt();
@@ -31,7 +28,6 @@ public class Main {
                 win = true;
                 System.out.println("Good job, " + name + "! You guessed my number in " + turns + " guess/es!");
                 System.out.println("Would you like to play again? (y/n)");
-//                playAgain = scan.next();
             }
             else if (guess<correctNumber) {
                 System.out.println("Your guess is lower than the number");
@@ -41,21 +37,6 @@ public class Main {
                 System.out.println("Your guess is higher than the number");
                 win=false;
             }
-
         }
-//        do {
-//            int theNumber = (int) (Math.random() * 100 + 1);
-//            while (numberGuess != theNumber) {
-//                System.out.println("Hello! What is your name?");
-//
-//                numberGuess = scan.nextInt();
-//
-//                if (numberGuess < theNumber)
-//                    System.out.println(numberGuess + " is too low. Take a guess.");
-//                else if (numberGuess > theNumber)
-//                    System.out.println(numberGuess + " is too high. Take a guess.");
-//                else System.out.println("you win");
-//            }
-//        }
     }
 }
